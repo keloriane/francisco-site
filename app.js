d9d1209eb8c218039ccba19e93106ce399245038
keloriane <kevin.flbt@gmail.com>
2019-08-19 11:19:15 +0200

    fixing minor bugs

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const profile = document.getElementById("fbl");
+const blueWrapper = document.getElementsByClassName('min-wrapper')
 
-TweenMax.from(profile, 1, { width: 0, opacity: 0 });
+
+
+TweenMax.from(profile, 1, { width: 0, delay:.5});
+TweenMax.staggerFrom(blueWrapper,0.5,{y:150,opacity:0, delay: 1.5, ease:Power2.easeOut,yoyo:true},0.1)
